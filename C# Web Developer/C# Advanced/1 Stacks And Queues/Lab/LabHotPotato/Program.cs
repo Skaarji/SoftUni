9b65be1f772ef4e4d92cc21ed923c4ee6a3b7ec9
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-06-25 22:36:05 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Lab/LabHotPotato/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabHotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            int toss = int.Parse(Console.ReadLine());
+            Queue<string> kids = new Queue<string>(names);
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < toss -1; i++)
+                {
+                    string currentkid = kids.Dequeue();
+                    kids.Enqueue(currentkid);
+                }
+                Console.WriteLine($"Removed {kids.Dequeue()}");
+            }
+            Console.WriteLine($"Last is {kids.Peek()}");
+
+        }
+    }
+}
