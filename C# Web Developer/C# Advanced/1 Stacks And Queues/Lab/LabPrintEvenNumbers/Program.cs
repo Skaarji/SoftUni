447902ebfdb0a6dbea40a94e10b6b59b23664c23
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-06-26 17:52:05 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Lab/LabPrintEvenNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LabPrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(array);
+            int count = queue.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = queue.Peek();
+                if (number % 2 == 0)
+                {
+                    queue.Dequeue();
+                    queue.Enqueue(number);
+                }
+                else
+                {
+                    queue.Dequeue();
+                }
+            }
+            Console.WriteLine(String.Join(", ", queue));
+        }
+    }
+}
