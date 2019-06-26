4977def400b85d27ab9b0ec70516bdcac893dfc4
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-06-26 17:52:36 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Lab/LabReverseStrings/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<Char> stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+            int count = stack.Count;
+            for (int i = 0; i < count ; i++)
+            {
+                Console.Write(stack.Pop());
+
+            }
+            //while (stack.Count != 0)
+            //{
+            //    Console.Write(stack.Pop());
+            //}
+            Console.WriteLine();
+
+
+        }
+    }
+}
