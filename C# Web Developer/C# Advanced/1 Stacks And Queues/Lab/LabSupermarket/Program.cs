b777852ebf536b24126aeb736207a16c51be53cc
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-06-28 22:04:25 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Lab/LabSupermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabSupermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Queue<string> customers = new Queue<string>();
+
+            while (input != "End")
+            {
+                if (input == "Paid" && customers.Count >0)
+                {
+                    while (customers.Count >0)
+                    {
+                        Console.WriteLine(customers.Dequeue());
+                    }
+                }
+                else
+                {
+                    customers.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{customers.Count} people remaining.");
+        }
+    }
+}
