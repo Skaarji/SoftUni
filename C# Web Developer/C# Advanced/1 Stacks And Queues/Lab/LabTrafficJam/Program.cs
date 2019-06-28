867933c606a3b8a0f5f6975c5755349211abbdea
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-06-28 22:04:57 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Lab/LabTrafficJam/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LabTrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int canPass = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            Queue<string> cars = new Queue<string>();
+            int totalCarsPassed = 0;
+            while (input != "end")
+            {
+                if (input =="green")
+                {
+                    int counter = 0;
+                    while (cars.Count >0 && counter < canPass)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        counter++;
+                        totalCarsPassed++;
+                    }
+                    counter = 0;                    
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
+        }
+    }
+}
