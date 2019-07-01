461cae85f6b8f0491cf5891672b3e8967692cb79
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-01 19:26:09 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExBasicQueueOperations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExBasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int numbersToEnqueue = numbers[0];
+            int numbersToDequeue = numbers[1];
+            int numberToLookFor = numbers[2];
+            if (numbersToDequeue == numbersToEnqueue)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            int[] forQueue = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> queue = new Queue<int>(forQueue);
+
+            for (int i = 0; i < numbersToDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Contains(numberToLookFor)==true)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+
+        }
+    }
+}
