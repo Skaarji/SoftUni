58e7dd4fbf45f58874c7671f02596109f9cbd3c3
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-01 19:25:50 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExBalancedParantheses/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExBalancedParantheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Stack<char> stackofPar = new Stack<char>();
+
+            char[] openPar = new char[] { '(', '{', '[' };
+
+            bool isValid = true;
+            foreach (var item in input)
+            {
+                if (openPar.Contains(item))
+                {
+                    stackofPar.Push(item);
+                    continue;
+                }
+                if (stackofPar.Count==0)
+                {
+                    isValid = false;
+                    break;
+                }
+                else if (stackofPar.Peek()== '(' && item == ')')
+                {
+                    stackofPar.Pop();
+                }
+                else if (stackofPar.Peek()== '{' && item == '}')
+                {
+                    stackofPar.Pop();
+                }
+                else if (stackofPar.Peek()== '[' && item == ']')
+                {
+                    stackofPar.Pop();
+                }
+                else
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+            if (isValid == true)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+
+        }
+    }
+}
