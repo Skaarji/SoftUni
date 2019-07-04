837b211062a33d54e3a19412e6cbdc951d6c9ab6
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-04 22:22:44 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExFashionBoutique/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExFashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>(clothes);
+            int rackCapacity = int.Parse(Console.ReadLine());
+            int rackCount = 1;
+            int CurrentRack = 0;
+
+            while (stack.Count>0)
+            {                
+                if ((CurrentRack + stack.Peek()) > rackCapacity)
+                {
+                    rackCount++;
+                    CurrentRack = 0;
+                    continue;
+                }
+                else if ((CurrentRack + stack.Peek()) <= rackCapacity)
+                {
+                    CurrentRack+=stack.Pop();
+                }
+            }
+            Console.WriteLine(rackCount);
+
+            
+        }
+    }
+}
