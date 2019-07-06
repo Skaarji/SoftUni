8c7c1cb4a5d32d7cebc8316984bb6d3ed8e1d567
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-06 23:34:01 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExFastFood/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExFastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantity = int.Parse(Console.ReadLine());
+            int[] numberSequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> orders = new Queue<int>(numberSequence);
+
+            Console.WriteLine(orders.Max());
+
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (foodQuantity>= orders.Peek())
+                {
+                    foodQuantity -= orders.Dequeue();
+                    i--;
+                    continue;
+                }
+                else if (foodQuantity< orders.Peek())
+                {
+                    break;
+                }
+            }
+            if (orders.Count==0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else if (orders.Count>0)
+            {
+                Console.Write("Orders left: ");
+                while (orders.Count>0)
+                {
+                    Console.Write($"{orders.Dequeue()} ");
+                }
+            }
+
+        }
+    }
+}
