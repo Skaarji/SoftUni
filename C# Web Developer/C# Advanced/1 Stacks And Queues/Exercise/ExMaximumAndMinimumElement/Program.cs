ac14a6a72cb715655882abbe9d2efa0c174b383f
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-06 23:34:42 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExMaximumAndMinimumElement/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExMaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (input[0]==1)
+                {
+                    stack.Push(input[1]);
+                }
+                else if (input[0]==2 && stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+                else if (input[0]==3 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Max());
+                }
+                else if (input[0] ==4 && stack.Count > 0)
+                {
+                    Console.WriteLine(stack.Min());
+                }
+                
+            }
+            Console.WriteLine(string.Join(", ", stack));
+        }
+    }
+}
