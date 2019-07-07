f8d85985cedb79061a0cee257c8b9aa68ee63159
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-07 22:21:35 +0300

    update

---
+++ C# Web Developer/C# Advanced/1 Stacks And Queues/Exercise/ExSimpleTextEditor/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExSimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = null;
+
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            //StringBuilder sb = new StringBuilder();
+
+            Stack<string> textStates = new Stack<string>();  
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                
+                string input = Console.ReadLine();
+                string[] command = input.Split();
+                int type = int.Parse(command[0]);
+
+                if (type == 1)
+                {
+                    textStates.Push(text);
+                    text += command[1];                    
+                }
+                else if (type == 2)
+                {
+                    textStates.Push(text);
+                    text = text.Substring(0, text.Length - int.Parse(command[1]));
+                    //Queue<char> textInChar= new Queue<char>(text.ToCharArray());
+                    //for (int j = 0; j < int.Parse(command[1]); j++)
+                    //{
+                    //    textInChar.Dequeue();
+                    //}
+                    //text = string.Join("", textInChar);
+                }
+                else if (type == 3)
+                {
+                    int index = int.Parse(command[1]);
+                    Console.WriteLine(text[index-1]);
+                }
+                else if (type== 4)
+                {
+                    text = textStates.Pop();
+                }
+            }
+
+        }
+    }
+}
