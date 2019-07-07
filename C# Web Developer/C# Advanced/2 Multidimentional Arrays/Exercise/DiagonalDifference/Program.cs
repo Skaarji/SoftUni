34e496a4868aa1543641d41723fb8e360e0a3579
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-07 22:26:43 +0300

    update

---
+++ C# Web Developer/C# Advanced/2 Multidimentional Arrays/Exercise/DiagonalDifference/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimentions = int.Parse(Console.ReadLine());
+            int[,] array = new int[dimentions, dimentions];
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = input[j];
+                }
+            }
+            int diag1 = 0;
+            int diag2 = 0;
+            for (int i = 0; i < dimentions; i++)
+            {
+                diag1 += array[i, i];
+                diag2 += array[i, dimentions - 1 - i];
+            }
+
+            Console.WriteLine(Math.Abs(diag1 - diag2));
+        }
+    }
+}
