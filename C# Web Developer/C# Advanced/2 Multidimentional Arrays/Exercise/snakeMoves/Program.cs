13adb37acbc40a22f42a91e24dc39d4083105db6
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-09 23:11:00 +0300

    update

---
+++ C# Web Developer/C# Advanced/2 Multidimentional Arrays/Exercise/snakeMoves/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace snakeMoves
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            char[,] matrix = new char[dimensions[0], dimensions[1]];
+            string snake = Console.ReadLine();
+            int counter = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+
+            {
+                for (int j = 0; j < matrix.GetLength(1) ; j++)
+                {
+                    if (counter >= snake.Length)
+                    {
+                        counter = 0;
+                    }
+                    matrix[i, j] = snake[counter++];
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+        }
+    }
+}
