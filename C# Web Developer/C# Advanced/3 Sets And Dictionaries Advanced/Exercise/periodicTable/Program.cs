8c8ae513914e3b010e9e141924654652a23aa0e1
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-10 21:07:45 +0300

    update

---
+++ C# Web Developer/C# Advanced/3 Sets And Dictionaries Advanced/Exercise/periodicTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace periodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            SortedSet<string> elements = new SortedSet<string>();
+            
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                List<string> compounds = Console.ReadLine().Split().ToList();
+                for (int j = 0; j < compounds.Count; j++)
+                {
+                    elements.Add(compounds[j]);
+                }
+            }
+            Console.WriteLine(string.Join(" ", elements));
+        }
+    }
+}
