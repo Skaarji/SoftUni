9a2f3f9def90017c4b670fca1fbf824360ba02e6
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-10 21:07:26 +0300

    update

---
+++ C# Web Developer/C# Advanced/3 Sets And Dictionaries Advanced/Exercise/evenTimes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace evenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (!dict.ContainsKey(input))
+                {
+                    dict.Add(input, 1);
+                }
+                else
+                {
+                    dict[input]++;
+                }
+            }
+            foreach (var item in dict)
+            {
+                if (item.Value % 2 == 0)
+                    Console.WriteLine(item.Key);
+            }
+        }
+    }
+}
