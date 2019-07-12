c7167eb3ca25bd45f645dc88571556db9d53555a
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-12 23:31:58 +0300

    update

---
+++ C# Web Developer/C# Advanced/3 Sets And Dictionaries Advanced/Exercise/UniqueUsernames/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            List<string> usernames = new List<string>();
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string input = Console.ReadLine();
+                if (!usernames.Contains(input))
+                {
+                    usernames.Add(input);
+                }
+            }
+            foreach (var item in usernames)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
