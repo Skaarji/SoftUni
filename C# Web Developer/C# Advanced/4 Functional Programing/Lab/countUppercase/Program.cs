66b0bc1b6ef5e48ad11f1f5e45c3c2f733d3af33
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-13 23:39:28 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Lab/countUppercase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace countUppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(Upper)
+                //.Where(x => char.IsUpper(x[0]))
+                .ToArray();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text[i]);
+            }               
+            
+            
+        }
+
+        public static Func<string, bool> Upper = n =>
+        {
+            bool result = false;
+            if (n[0] > 64 && n[0]< 90)
+            {
+                result = true;
+                return result;
+            }
+            return result;
+
+        };
+    }
+}
