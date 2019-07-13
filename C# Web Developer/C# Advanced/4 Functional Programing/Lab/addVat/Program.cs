4265f51396e32076f271f9ad3f32e1ffb6f790fa
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-13 23:38:51 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Lab/addVat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace addVat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(x => (x * 1.2))
+                .ToArray();
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                Console.WriteLine($"{prices[i]:f2}");
+            }
+        } 
+    }
+}
