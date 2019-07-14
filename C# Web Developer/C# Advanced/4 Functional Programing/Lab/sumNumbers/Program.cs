df973e7afc3e0eb4456cb286d1a80f6a05234867
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-14 22:45:58 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Lab/sumNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace sumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries).Select(Parse).ToArray();           
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+
+       public static Func<string, int> Parse = n =>
+       {
+           int result = 0;
+           if (Int32.TryParse(n, out result))
+           {
+               return result;
+           }
+           return 0;
+       };
+    }
+}
