27e2649325d659135a70c2df93b8c719db92d337
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-15 23:52:31 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/actionPoint/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace actionPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            foreach (var item in names)
+            {
+                Print(item);
+            }
+        }
+        public static Action<string> Print = n => Console.WriteLine(n);
+        
+        
+    }
+}
