9d511664256508e4da7d03222e641e91ded70a9e
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-15 23:52:48 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/appliedArithmetics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace appliedArithmetics
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                if (input == "multiply")
+                {
+                    numbers = numbers.Select(x => x * 2).ToArray();
+                }
+                else if (input=="add")
+                {
+                    numbers = numbers.Select(x => x + 1).ToArray();
+                }
+                else if (input == "subtract")
+                {
+                    numbers = numbers.Select(x => x - 1).ToArray();
+                }
+                else if (input == "print")
+                {
+                    Console.WriteLine(string.Join(" ",numbers));
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
