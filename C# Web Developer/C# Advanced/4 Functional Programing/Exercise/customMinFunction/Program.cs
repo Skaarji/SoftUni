2eed45f2654061de7db3a58dc0f7d3a221bf8e73
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-16 22:51:35 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/customMinFunction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace customMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Func<int[], int> smallest = num =>
+             {
+                 int minvalue = int.MaxValue;
+                 foreach (var n in num)
+                 {
+                     if (n < minvalue)
+                     {
+                         minvalue = n;
+                     }                     
+                 }
+                 return minvalue;
+             };
+            Console.WriteLine(smallest(numbers));
+        }
+    }
+}
