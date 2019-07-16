44b879eae08f9070bf3e4e917d3d36ee0896c916
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-16 22:52:21 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/knightsOfHonor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace knightsOfHonor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] knights = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => "Sir"+" "+x)
+                .ToArray();
+
+            foreach (var item in knights)
+            {
+                Print(item);
+            }
+            
+        }
+        public static Action<string> Print = x => Console.WriteLine(x);
+    }
+}
