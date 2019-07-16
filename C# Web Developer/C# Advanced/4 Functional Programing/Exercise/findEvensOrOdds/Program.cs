182ef16d2b73a907fcc1b3589a0a4c157deb48cc
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-16 22:51:56 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/findEvensOrOdds/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace findEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bounds = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int lower = bounds[0];
+            int upper = bounds[1];
+            List<int> numbers = new List<int>();
+
+            string type = Console.ReadLine();
+
+            for (int i = lower; i <= upper; i++)
+            {
+                numbers.Add(i);
+            }
+
+            Predicate<int> isEven = number =>
+            {
+
+                if (number % 2 == 0)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            Predicate<int> isOdd = number => number % 2 != 0;
+
+            numbers =numbers.Where(x => type == "odd" ? isOdd(x) : isEven(x)).ToList();
+
+            Console.WriteLine(string.Join(" ",numbers));
+
+        }
+    }
+}
