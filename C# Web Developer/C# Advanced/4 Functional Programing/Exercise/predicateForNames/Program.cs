976d5c67faf3667fb97d298cee9e3bd477d910f9
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-18 22:53:52 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/predicateForNames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace predicateForNames
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int desiredLenght = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length <= desiredLenght)
+                .ToArray();
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
