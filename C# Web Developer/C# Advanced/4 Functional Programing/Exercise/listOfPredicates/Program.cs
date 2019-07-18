c8039dd6054b1d15bb013ed190195e49be79df20
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-18 22:53:07 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/listOfPredicates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace listOfPredicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, int.Parse(Console.ReadLine())).ToList();
+
+            int[] dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Distinct()
+                .ToArray();
+
+            for (int i = 0; i < dividers.Length; i++)
+            {
+                Predicate<int> divisible = x =>
+                {
+                    if (x % dividers[i] == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                    
+                };
+                numbers = numbers.FindAll(divisible);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
