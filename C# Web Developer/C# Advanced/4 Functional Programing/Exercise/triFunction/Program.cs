3dbcd96125e4c0ddeb8af3b5fd9a0752ec0fed5d
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-20 20:23:32 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/triFunction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace triFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int desiredNumber = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Func<string, int, bool> firstFunction = (x, y) =>
+            {
+                int sumCharacters = 0;
+                for (int i = 0; i < x.Length; i++)
+                {
+                    sumCharacters += x[i];
+                }
+                if (sumCharacters >= y)
+                {
+                    return true;
+                }
+                return false;
+            };
+
+            names = names.Where(x => firstFunction(x, desiredNumber)).ToList();
+
+            Console.WriteLine(names.FirstOrDefault());
+        }
+    }
+}
