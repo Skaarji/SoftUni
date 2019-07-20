7ede1d023440b3bce5060e0344d7cb2a7fcf714b
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-20 20:23:10 +0300

    update

---
+++ C# Web Developer/C# Advanced/4 Functional Programing/Exercise/reverseAndExclude/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace reverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Reverse()
+                .ToArray();
+            int divisor = int.Parse(Console.ReadLine());
+            numbers = numbers.Where(x => x % divisor !=0).ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
