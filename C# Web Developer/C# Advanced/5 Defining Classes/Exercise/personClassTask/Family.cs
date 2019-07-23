ee8a0751cf1608d19a2614d160b4eb25a597994e
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-23 23:05:10 +0300

    update

---
+++ C# Web Developer/C# Advanced/5 Defining Classes/Exercise/personClassTask/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> family;
+
+        public Family()
+        {
+           family = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+           family.Add(member);
+        }
+
+        public Person GetOldest()
+        {
+            var tempFamily = family.OrderByDescending(x => x.Age).ToList();
+            return tempFamily[0];
+        }
+    }
+}
---
+++ C# Web Developer/C# Advanced/5 Defining Classes/Exercise/personClassTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < number; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                Person person = new Person(input[0], int.Parse(input[1]));
+                people.Add(person);               
+            }
+            people = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+            foreach (var item in people)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
+            
+        }
+    }
+}
