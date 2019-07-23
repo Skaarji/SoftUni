fbb212ef393e3910602ce034a127a39f924a17e1
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-23 23:04:47 +0300

    update

---
+++ C# Web Developer/C# Advanced/5 Defining Classes/Exercise/DateModifierTask/DateModifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public string Date1 { get; set; }
+
+        public string Date2 { get; set; }
+
+
+
+        public int difference(string date1, string date2)
+        {
+            int[] firstDate = date1.Split(" ").Select(int.Parse).ToArray();
+            int[] secondDate = date2.Split(" ").Select(int.Parse).ToArray();
+            DateTime dateOne = new DateTime(firstDate[0],firstDate[1],firstDate[2]);
+            DateTime dateTwo = new DateTime(secondDate[0],secondDate[1],secondDate[2]);
+
+            TimeSpan dif = dateOne - dateTwo;
+
+            return Math.Abs((dateOne - dateTwo).Days);
+
+            
+        }
+
+    }
+}
---
+++ C# Web Developer/C# Advanced/5 Defining Classes/Exercise/DateModifierTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+
+            DateModifier dif = new DateModifier();
+            int result = dif.difference(dateOne, dateTwo);
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
