26dcc4cf26379076db3e493d188ad322d0c0464d
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-26 18:04:48 +0300

    update

---
+++ C# Web Developer/C# OOP/1 Working With Abstraction/Lab/LabPointInRectangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace LabPointInRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] coordinates = ParseInput();
+            int numberOfLines = int.Parse(Console.ReadLine());
+
+            Point topLeft = new Point(coordinates[0], coordinates[1]);
+            Point bottomRight = new Point(coordinates[2], coordinates[3]);
+
+            Rectangle rectangle = new Rectangle(topLeft, bottomRight);
+
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                int[] point = ParseInput();
+
+                Point currentPoint = new Point(point[0], point[1]);
+
+                Console.WriteLine(rectangle.Contains(currentPoint));
+            }
+
+
+        }
+
+        private static int[] ParseInput()
+        {
+            return Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ C# Web Developer/C# OOP/1 Working With Abstraction/Lab/LabPointInRectangle/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabPointInRectangle
+{
+    class Rectangle
+    {
+        public Point TopLeft { get; set; }
+        public Point BottomRight { get; set; }
+
+        public Rectangle(Point topleft, Point bottomright)
+        {
+            TopLeft = topleft;
+            BottomRight = bottomright;
+        }
+
+        public bool Contains(Point point)
+        {
+            bool insideByX = point.CoordinateX >= TopLeft.CoordinateX && point.CoordinateX <= BottomRight.CoordinateX;
+            bool insideByY = point.CoordinateY >= TopLeft.CoordinateY && point.CoordinateY <= BottomRight.CoordinateY;
+
+            return insideByX && insideByY;
+        }
+
+    }
+}
