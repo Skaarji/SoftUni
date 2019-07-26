4d633951ad385e4bd15c8823ecd0238d5676442c
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-26 18:04:14 +0300

    update

---
+++ C# Web Developer/C# OOP/1 Working With Abstraction/Lab/LabHotelReservations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabHotelReservations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
