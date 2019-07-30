193fedbe4ff300cde8b3afb46c7656a2ca1deb2b
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-30 20:43:33 +0300

    update

---
+++ C# Web Developer/C# OOP/2 Inheritance/Lab/LabSingleInheritance/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabSingleInheritance
+{
+   public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
