a7444e88ba7b9c92b4c1a16f5e25240d1a7becf1
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-07-31 23:16:35 +0300

    update

---
+++ C# Web Developer/C# OOP/3 Encapsulation/Lab/LabFirstAndReserveTeam/Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private int age;
+        private string lastName;
+        private string firstName;
+        private decimal salary;
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstName = value;
+            }
+        }
+
+
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastName = value;
+            }
+
+
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            private set
+            {
+                if (value<460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+        }
+
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal increaase)
+        {
+            if (Age < 30)
+            {
+                Salary += Salary * increaase / 200;
+            }
+            else
+            {
+                Salary += Salary * increaase / 100;
+            }
+        }
+
+    }
+}
---
+++ C# Web Developer/C# OOP/3 Encapsulation/Lab/LabFirstAndReserveTeam/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            while (n-->0)
+            {
+                string[] token = Console.ReadLine().Split();
+                Person p = new Person(token[0], token[1], int.Parse(token[2]), decimal.Parse(token[3]));
+                people.Add(p);
+            }
+            
+            Team team = new Team("SoftUni");
+
+            foreach (Person person in people)
+            {
+                team.AddPlayer(person);
+            }
+
+            Console.WriteLine($"First team has {team.FirstTeam.Count} players.");
+            Console.WriteLine($"Reserve team has {team.ReserveTeam.Count} players.");
+
+        }
+    }
+}
