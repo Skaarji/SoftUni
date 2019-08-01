4dfc27a4b88b7072d53ac3788c48978f75d41c6c
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-08-01 21:13:23 +0300

    update

---
+++ C# Web Developer/C# OOP/3 Encapsulation/Exercise/ExAnimalFarm/Exceptions/ExceptionsText.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnimalFarm.Exceptions
+{
+    class ExceptionsText
+    {
+        public static string NameCannotBeEmpty= "Name cannot be empty.";
+        public static string AgeInvalid= "Age should be between 0 and 15.";
+        
+    }
+}
