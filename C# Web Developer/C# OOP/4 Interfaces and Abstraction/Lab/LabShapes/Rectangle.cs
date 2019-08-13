ed099aaa1918100d3d0a61618c8b46b491f60b03
Blagoy Dzhiganski <48655836+Skaarji@users.noreply.github.com>
2019-08-13 20:25:16 +0300

    update

---
+++ C# Web Developer/C# OOP/4 Interfaces and Abstraction/Lab/LabShapes/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Rectangle : IDrawable
+    {
+        private readonly int width;
+        private readonly int height;
+
+        public Rectangle(int width, int height)
+        {
+            this.height = height;
+            this.width = width;
+        }
+        
+        public void Draw()
+        {
+            DrawLine('*', '*');
+            for (int i = 0; i < height-2; i++)
+            {
+                DrawLine('*', ' ');
+            }
+            DrawLine('*', '*');
+        }
+
+        private void DrawLine(char borderSymbol, char innerSymbol)
+        {
+            Console.Write(borderSymbol);
+            for (int i = 0; i < width-2; i++)
+            {
+                Console.Write(innerSymbol);
+            }
+            Console.WriteLine(borderSymbol);
+        }
+    }
+}
